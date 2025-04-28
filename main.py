169aacd9d56c4ff09eaa968074e4865f3fa25873
jimmyswarper <jimswarper@gmail.com>
2025-04-28 18:09:31 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Engloding Starter Code
+
+user_input = input("Type your Engloding command: ")
+
+if "press a button" in user_input.lower():
+    print("You got some random stuff!")
+else:
+    print("I don't know what you meant, but cool!")
